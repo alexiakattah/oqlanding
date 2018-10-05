ab3bece55fea114068af477c1d75792dfdcd4d72
Rodrigo H. A. Xavier <rodrigoxavieer@Rodrigos-MacBook-Pro.local>
2018-10-05 16:45:47 -0300

    Criando os circulos

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	"use strict";
+	/*
+	 * ----------------------------------------------------------------------------------------
+	 *  CHANGE MENU BACKGROUND JS
+	 * ----------------------------------------------------------------------------------------
+	 */
+
+	var headertopoption = $(window);
+	var headTop = $('.header-top-area');
+	headertopoption.on('scroll', function() {
+		if (headertopoption.scrollTop() > 200) {
+			headTop.addClass('menu-bg');
+		} else {
+			headTop.removeClass('menu-bg');
+		}
+	});
+
+	
+
+
+	/*==============================================================
+	 wow animation - on scroll
+	 ==============================================================*/
+	var wowJs = $('.wow');
+	if (wowJs.length) {
+		var wow = new WOW({
+			boxClass : 'wow',
+			animateClass : 'animated',
+			offset : 0,
+			mobile : false,
+			live : true
+		});
+
+		wow.init();
+		// WOW JS
+		new WOW({
+			mobile : false
+		}).init();
+	}
+	/*==============================================================
+	 set parallax
+	 ==============================================================*/
+	function stellarParallax() {
+		if ($(window).width() > 1024) {
+			$.stellar();
+		} else {
+			$.stellar('destroy');
+			$('.parallax').css('background-position', '');
+		};
+	};
+	
+	
+});
